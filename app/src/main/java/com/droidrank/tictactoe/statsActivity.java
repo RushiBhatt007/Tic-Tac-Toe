92b093b1434fcc6b91f8c79402d4b73890875ad2
Rushi Bhatt <rybhatt27@gmail.com>
2018-07-24 00:29:51 +0530

    graphs added in statsActivity

--- app/src/main/java/com/droidrank/tictactoe/statsActivity.java
+++ app/src/main/java/com/droidrank/tictactoe/statsActivity.java
@@ -2,11 +2,22 @@ package com.droidrank.tictactoe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class statsActivity extends AppCompatActivity
 {
+    LineChart lineChart;
     int winX,winO,draw;
+    int colors[]= {R.color.myred, R.color.myblue, R.color.myyellow };
     @Override
     public void onCreate(Bundle savedInstance)
     {
@@ -17,18 +28,42 @@ public class statsActivity extends AppCompatActivity
 
         setContentView(R.layout.stats_screen);
 
-        if(tempX=="")
-            tempX="0";
-        if(tempO=="")
-            tempO="0";
-        if(tempDraw=="")
-            tempDraw="0";
-
         winX = Integer.parseInt(tempX);
         winO = Integer.parseInt(tempO);
         draw = Integer.parseInt(tempDraw);
 
-        TextView textView = (TextView) findViewById(R.id.textview);
-        textView.setText("WIN OF X IS "+winX+"\n"+"WIN OF O IS "+winO+"\n"+"DRAW IS "+draw+"\n");
+        lineChart = (LineChart) findViewById(R.id.graph);
+
+        List<Entry> entries = new ArrayList<Entry>();
+        entries.add(new Entry(0f,winX));
+        entries.add(new Entry(1f,winO));
+        entries.add(new Entry(2f,draw));
+
+        LineDataSet lineDataSet = new LineDataSet(entries,"Statistical representation of result");
+        lineDataSet.setColors(colors, getApplicationContext());
+        lineDataSet.setCubicIntensity(100);
+        lineDataSet.setMode(LineDataSet.Mode.HORIZONTAL_BEZIER);
+        lineDataSet.setDrawFilled(true);
+
+        LineData lineData = new LineData(lineDataSet);
+
+        XAxis xAxis = new XAxis();
+        YAxis yAxis = new YAxis();
+        xAxis.setEnabled(false);
+        yAxis.setEnabled(false);
+        xAxis.setDrawAxisLine(false);
+        yAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        yAxis.setDrawGridLines(false);
+
+        lineChart.getXAxis().setEnabled(false);
+        lineChart.getAxisRight().setDrawGridLines(false);
+        lineChart.getAxisLeft().setDrawAxisLine(false);
+        lineChart.getXAxis().setDrawGridLines(false);
+        lineChart.getXAxis().setDrawAxisLine(false);
+
+        lineChart.setData(lineData);
+        lineChart.animateXY(2000,2000);
+        lineChart.invalidate(); // refresh
     }
 }
